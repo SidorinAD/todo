c8790cefefff3d519509348aa54e79dd8925c29b
Alexander Sidorin <a.d.sidorin@gmail.com>
2021-08-27 12:39:50 +0300

    TodoItem actions added + styling + RobotoFont

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,14 +1,37 @@
-import { Button, Checkbox, Box } from "@material-ui/core"
+import { Checkbox, Grid, Box } from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
+import { observer } from "mobx-react-lite";
+import { useContext } from "react";
+import { TodoContext } from "../..";
 
-export const TodoItem = ({todo} : any) => {
-    const {id, title, completed} = todo
+export const TodoItem = observer(({ todoData }: any) => {
+  const todo = useContext(TodoContext);
+  const { id, title, completed } = todoData;
+  console.log(todoData)
   return (
-    <Box display="flex">
-      <p>{id}</p>
-      <p>{title}</p>
-      <Checkbox checked={completed}/>
-      <Button onClick={() => todo.removeTodo(id)}>X</Button>
-    </Box>
+    <Grid container direction="row" spacing={3}>
+      <Grid item xs={12}>
+        <Paper>
+          <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            p={1}
+          >
+            <Typography>{id}</Typography>
+            <Typography>{title}</Typography>
+            <Checkbox
+              checked={completed}
+              color="primary"
+              onClick={() => todo.completeTodo(id)}
+            />
+            <Delete onClick={() => todo.removeTodo(id)}></Delete>
+          </Box>
+        </Paper>
+      </Grid>
+    </Grid>
   );
-};
+});
--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react-lite"
+import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import { useContext } from "react";
 
@@ -7,20 +7,17 @@ import { TodoContext } from "../..";
 
 import { TodoItem } from "../TodoItem";
 
-
-
 export const TodoList = observer(() => {
-    const todo = useContext(TodoContext);
-    useEffect(() => {
-        todo.fetchTodos();
-    }, [todo])
-    
+  const todo = useContext(TodoContext);
+  useEffect(() => {
+    todo.fetchTodos();
+  }, [todo]);
+
   return (
     <Container>
-      {todo.todos.map(todo => (
-          <TodoItem todo={todo} key={todo.id} />
+      {todo.todos.map((todo) => (
+        <TodoItem todoData={todo} key={todo.id} />
       ))}
     </Container>
   );
 });
-
